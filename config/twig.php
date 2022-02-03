94393bd65834fe7694da91bc4db2de7013783159
24628 <merlijn1busch@gmail.com>
2022-02-03 18:57:21 +0100

    added new files

---
+++ config/twig.php
@@ -0,0 +1,3 @@
+<?php
+
+Use Symfony\Component\TwigConfig;
--- src/Http/Controller/LeapYearController.php
+++ src/Http/Controller/LeapYearController.php
@@ -5,16 +5,26 @@ namespace Http\Controller;
 use Http\Model\LeapYear;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class LeapYearController
+class LeapYearController extends AbstractController
 {
     public function index(Request $request, $year): Response
     {
         $leapYear = new LeapYear();
+
         if ($leapYear->isLeapYear($year)) {
-            return new Response('Yep, this is a leap year!');
+            $result = 'Yep, this is a leap year!';
+        } else {
+            $result ='Nope, this is not a leap year.';
         }
 
-        return new Response('Nope, this is not a leap year.');
+        return $this->render('user/notifications.html.twig', [
+            // this array defines the variables passed to the template,
+            // where the key is the variable name and the value is the variable value
+            // (Twig recommends using snake_case variable names: 'foo_bar' instead of 'fooBar')
+            'name' => "john doe",
+            'result' => $result,
+        ]);
     }
 }
