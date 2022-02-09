f39412c047bd9204c12d8667f177540c696d38a9
Merlijn Busch <merlijn1busch@gmail.com>
2022-02-09 19:26:01 +0100

    removed testing code

--- src/route.php
+++ src/route.php
@@ -2,13 +2,10 @@
 
 use App\Http\Controller\Admin\AdminMainController;
 use App\Http\Controller\Auth\LoginController;
-use App\Http\Controller\LeapYearController;
 use Matrix\Managers\RouteManager;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 $routes = [
-    ["name" => "leap_year", "url" => "/is_leap_year/{year}", "controller" => [new LeapYearController(), 'index'], "method" => "GET"],
-    ["name" => "leap_year_json", "url" => "/is_leap_year_json/{year}", "controller" => [new LeapYearController(), 'jsonTest'], "method" => "GET"],
     ["name" => "login", "url" => "/login", "controller" => [new LoginController(), 'index'], "method" => "GET"],
     ["name" => "login_post", "url" => "/login", "controller" => [new LoginController(), 'login'], "method" => "POST"],
     ["name" => "admin", "url" => "/admin", "controller" => [new AdminMainController(), 'index'], "method" => "GET"],
