cd729b2cfba34d4c1cfce483e930edd0cb681b83
Merlijn Busch <merlijn1busch@gmail.com>
2022-02-13 23:24:31 +0100

    start of schedule skip week functionality

--- resources/js/components/admin/EventSubNavigation.js
+++ resources/js/components/admin/EventSubNavigation.js
@@ -1,7 +1,7 @@
 class EventSubNavigation extends HTMLElement {
     constructor() {
         super();
-        this.attachShadow({ mode: "open" });
+        this.attachShadow({mode: "open"});
 
         this._$currentTime = new Date()
         this._$root = null;
@@ -35,7 +35,7 @@ class EventSubNavigation extends HTMLElement {
                     padding: 0 35px;
                 }
                 
-                .actions {
+                .helpers {
                     display: flex;
                     align-items: center;
                     justify-content: center;
@@ -62,10 +62,35 @@ class EventSubNavigation extends HTMLElement {
                     margin-right: 15px;
                 }
                 
+                .actions {
+                    justify-content: center;
+                    align-items: center;
+                    display: flex;
+                }
+                
+                .actions-icons {
+                    width: 35px;
+                    height: 35px;
+                }
+                
             </style>
             <div class="navigation">
                 <div class="title">${this.title} - Programs overview</div>
                 <div class="actions">
+<!--                    Week back-->
+                    <div><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="actions-icons bi bi-caret-left-fill" viewBox="0 0 16 16">
+                         <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"/>
+                    </svg></div>
+<!--                    calender-->
+                    <div><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="actions-icons bi bi-calendar-day-fill" viewBox="0 0 16 16">
+  <path d="M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4V.5zM16 14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V5h16v9zm-4.785-6.145a.428.428 0 1 0 0-.855.426.426 0 0 0-.43.43c0 .238.192.425.43.425zm.336.563h-.672v4.105h.672V8.418zm-6.867 4.105v-2.3h2.261v-.61H4.684V7.801h2.464v-.61H4v5.332h.684zm3.296 0h.676V9.98c0-.554.227-1.007.953-1.007.125 0 .258.004.329.015v-.613a1.806 1.806 0 0 0-.254-.02c-.582 0-.891.32-1.012.567h-.02v-.504H7.98v4.105z"/>
+</svg></div>
+<!--                    week forward-->
+                    <div><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="actions-icons bi bi-caret-right-fill" viewBox="0 0 16 16">
+                         <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+                    </svg></div>
+                </div>
+                <div class="helpers">
                     <div class="btn">?</div>
                     <div class="time">${this.getDateString()}</div>
                 </div>
@@ -78,9 +103,14 @@ class EventSubNavigation extends HTMLElement {
         this._$btn.addEventListener("click", this.handleBtnClick);
     }
 
-    getDateString(){
+    getDateString() {
         const date = new Date();
-        let dateString = date.toLocaleDateString('en-us', { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' }) + " " +
+        let dateString = date.toLocaleDateString('en-us', {
+                weekday: 'long',
+                year: 'numeric',
+                month: 'short',
+                day: 'numeric'
+            }) + " " +
             ("00" + date.getHours()).slice(-2) + ":" +
             ("00" + date.getMinutes()).slice(-2);
         return dateString.replace(/,/g, "");
@@ -96,7 +126,9 @@ class EventSubNavigation extends HTMLElement {
         this._$btn.removeEventListener("click", this.handleBtnClick, false);
     }
 
-    static get observedAttributes() { return ["title"]; }
+    static get observedAttributes() {
+        return ["title"];
+    }
 }
 
 export default EventSubNavigation;
