f498aef40a782e8143e43e90c9c95416f85e2b09
FlorisBeentjes <666792@student.inholland.nl>
2022-02-17 11:36:35 +0100

    Worked on email controller

--- resources/views/partials/tests/test_email.blade.php
+++ resources/views/partials/tests/test_email.blade.php
@@ -3,7 +3,7 @@
     <h1>Email-Test</h1>
 
     <form action="" method="post">
-        <input type="hidden">
+        <input type = "hidden">
         Email: <input type="email" name="Email address of the recipient:" required><br>
         Message: <input type="text" name="Message:" required><br>
         <input type="submit">
--- src/App/Http/Controller/TestEmailController.php
+++ src/App/Http/Controller/TestEmailController.php
@@ -8,6 +8,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
+use Symfony\Component\Mailer\Transport;
+use Symfony\Component\Mailer\Mailer;
 
 class TestEmailController extends BaseController
 {
@@ -20,8 +22,11 @@ class TestEmailController extends BaseController
         return $this->render("partials.tests.test_email", []);
     }
 
-    public function sendEmail(MailerInterface $mailer, $recipient)
+    public function sendEmail($recipient)
     {
+        $transport = Transport::fromDsn('smtp://localhost');
+        $mailer = new Mailer($transport);
+
         $email = (new Email())
             ->from('info@haarlemfestival.com')
             ->to($recipient)
@@ -29,8 +34,8 @@ class TestEmailController extends BaseController
             //->bcc('bcc@example.com')
             //->replyTo('fabien@example.com')
             //->priority(Email::PRIORITY_HIGH)
-            ->subject('Test Successfully!')
-            ->text('Yow!');
+            ->subject('Haarlem Festival Mail!')
+            ->text('Lets go!');
 
         $mailer->send($email);
     }
