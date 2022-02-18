42f37a9ad257a2954ae29a991c83bcb3265bf720
Merlijn Busch <merlijn1busch@gmail.com>
2022-02-18 14:36:52 +0100

    example queries

--- tests/RestaurantTypes.php
+++ tests/RestaurantTypes.php
@@ -4,7 +4,10 @@ use App\Model\Restaurant;
 
 require_once dirname(__DIR__, 1)."/Boot.php";
 
-$res = Restaurant::find(1);
+$res = Restaurant::find(1); // Just finds first items
+//$res = Restaurant::findOrFail(1); // Fails if it cannot find the item
+//$res = Restaurant::query()->where("id", "=", 1)->first(); // Queries the collection and takes the first item
+//$res = Restaurant::query()->where("id", "=", 1)->get()[0];// Queries the collection and take all items
 
 foreach ($res->types as $type) {
     var_dump($type->type);
