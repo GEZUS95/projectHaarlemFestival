65ac48f7536f4dcd244fbd0191f41c4fbe7fae1a
GEZUS95 <ceesgribnau@hotmail.com>
2022-02-24 16:02:57 +0100

    .

--- resources/views/partials/restaurant/index.blade.php
+++ resources/views/partials/restaurant/index.blade.php
@@ -2,8 +2,19 @@
 @section('content')
     <h1>Food</h1>
 
-    @foreach($restaurant as $restaurant)
-        <div>{{$restaurant}}</div>
+    @foreach($restaurant as $res)
+        <article class="card">
+            <div class="card-body">
+            <h3>{{$res->name}}</h3>
+            <p>{{$res->stars}}</p>
+            <p>cuisines:
+                @foreach($res->types as $type)
+                    {{$type->type}}
+                @endforeach
+            </p>
+            <p>Tickets left: {{$res->seats}}</p>
+            </div>
+        </article>
     @endforeach
 
 @endsection
