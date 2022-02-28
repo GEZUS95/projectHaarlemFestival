d7d6d758df0b4d4541f3d7b58dee0997235e16af
FlorisBeentjes <666792@student.inholland.nl>
2022-02-28 21:43:48 +0100

    we have signup-email

--- resources/views/emails/signup.blade.php
+++ resources/views/emails/signup.blade.php
@@ -1,6 +1,9 @@
 <html>
-<h1 style="color:blue;text-align:center;">Thank you for signing up for the Haarlem Festival site, Testpersoon!</h1>
+<body style="margin: 0;padding: 0;"><table class="wrapper"><tr><td style="text-align:center;"></td></tr></table><table class="main" style="text-align: left; margin-left: auto; margin-right: auto;"></table>
+<h1 style="font-size:xx-large;color:white;background-color:#1A222A;text-align:center;padding:1%;">Haarlem Festival</h1>
+<h1 style="background-color:white;color:black;text-align:center;padding:20px;">Thank you for signing up for the Haarlem Festival site, Testpersoon!</h1>
 
-
-<p style="color:red;text-align:center;">We will keep you posted on the latest festival tips, program updates, news and special appearances.</p>
+<p style="background-color:white;color:#222222;text-align:center;padding:20px;">We will keep you posted on the latest festival tips, program updates, news and special appearances.<br><br>Thank you for signing up,<br>The Haarlem Festival Team</p>
+<p style="font-size: x-small;background-color:#ECEFF1;text-align:center;padding:2%">This email was sent to you to inform that your account is successfully created.<br>The email-address that this email has been sent from cannot receive replies.<br>For help, visit: www.haarlemfestival.com/help</p>
+</body>
 </html>
