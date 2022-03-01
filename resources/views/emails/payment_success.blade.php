a8d29d729afcc89d4edce72d9f7709269f87de87
FlorisBeentjes <666792@student.inholland.nl>
2022-03-01 13:36:55 +0100

    Order mail complete

---
+++ resources/views/emails/payment_success.blade.php
@@ -0,0 +1,12 @@
+<html>
+<body style="margin: 0;padding: 0;"><table class="wrapper"><tr><td style="text-align:center;"></td></tr></table><table class="main" style="text-align: left; margin-left: auto; margin-right: auto;"></table>
+<h1 style="font-size:xx-large;color:white;background-color:#1A222A;text-align:center;padding:1%;">Haarlem Festival</h1>
+<h1 style="background-color:white;color:black;text-align:center;padding:20px;">Hi Testpersoon!</h1>
+
+<p style="background-color:white;color:#222222;text-align:center;">Thanks for your order!<br>This is your confirmation email and a quick summary of the tickets you ordered!<br>Your tickets will be below in the form of a QR-code!</p>
+<p style="background-color:white;color:#222222;text-align:center;padding:20px;">Here goes a quick overview of someone's order but yeah I guess that it is easier to yoink the code that we will use on the main page for this</p>
+<p style="background-color:white;color:#222222;text-align:center;padding:20px;">Below you will find unique QR-code, you will be able to use this code at the entrance of any event.<br>Put the generated qr-cde here and we gucci</p>
+
+<p style="font-size: x-small;background-color:#ECEFF1;text-align:center;padding:2%">This email was sent to you to inform that an order has been placed using your account.<br>The email-address that this email has been sent from cannot receive replies.<br>Don't want automated emails? Click <a href="www.haarlemfestival.com/opt_out">here</a> to opt out<br>For help, visit: www.haarlemfestival.com/help</p>
+</body>
+</html>
--- resources/views/emails/signup.blade.php
+++ resources/views/emails/signup.blade.php
@@ -4,6 +4,6 @@
 <h1 style="background-color:white;color:black;text-align:center;padding:20px;">Thank you for signing up for the Haarlem Festival site, Testpersoon!</h1>
 
 <p style="background-color:white;color:#222222;text-align:center;padding:20px;">We will keep you posted on the latest festival tips, program updates, news and special appearances.<br><br>Thank you for signing up,<br>The Haarlem Festival Team</p>
-<p style="font-size: x-small;background-color:#ECEFF1;text-align:center;padding:2%">This email was sent to you to inform that your account is successfully created.<br>The email-address that this email has been sent from cannot receive replies.<br>For help, visit: www.haarlemfestival.com/help</p>
+<p style="font-size: x-small;background-color:#ECEFF1;text-align:center;padding:2%">This email was sent to you to inform that your account is successfully created.<br>The email-address that this email has been sent from cannot receive replies.<br>Don't want automated emails? Click <a href="www.haarlemfestival.com/opt_out">here</a> to opt out<br>For help, visit: www.haarlemfestival.com/help</p>
 </body>
 </html>
