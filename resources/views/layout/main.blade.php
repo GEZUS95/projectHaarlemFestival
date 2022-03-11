b4fd6e994d59cd344f9813c0ac0f5ecf5c5d4deb
GEZUS95 <ceesgribnau@hotmail.com>
2022-03-11 15:50:20 +0100

    disbled caching

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -5,6 +5,10 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+{{--    disable caching     --}}
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
 
     <link rel="stylesheet" href="{{\Matrix\Managers\RouteManager::getUrlByRouteName("css")}}">
     <title>Haarlem Festival</title>
