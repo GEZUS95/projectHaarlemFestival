b8eb94832dd2588dba8675757beffd6873f603bd
GEZUS95 <ceesgribnau@hotmail.com>
2022-03-11 15:51:08 +0100

    .

--- resources/views/partials/restaurant/index.blade.php
+++ resources/views/partials/restaurant/index.blade.php
@@ -3,16 +3,17 @@
     <h1>Food</h1>
 
     @foreach($restaurant as $res)
-        <a href="{{\Matrix\Managers\RouteManager::getUrlByRouteName("restaurant_single", ["id" => $res->id])}}" class="card">
-            <div class="card-body">
-            <h3>{{$res->name}}</h3>
-            <p>{{$res->stars}}</p>
-            <p>cuisines:
-                @foreach($res->types as $type)
-                    {{$type->type}}
-                @endforeach
-            </p>
-            <p>Tickets left: {{$res->seats}}</p>
+        <a href="{{\Matrix\Managers\RouteManager::getUrlByRouteName("restaurant_single", ["id" => $res->id])}}"
+           class="partials-food-index-card">
+            <div class="partials-food-index-card-body">
+                <h3>{{$res->name}}</h3>
+                <p>{{$res->stars}}</p>
+                <p>cuisines:
+                    @foreach($res->types as $type)
+                        {{$type->type}}
+                    @endforeach
+                </p>
+                <p>Tickets left: {{$res->seats}}</p>
             </div>
         </a>
     @endforeach
