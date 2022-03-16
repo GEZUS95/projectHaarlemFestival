110d59b73f76fe19ad2719f6ccdc1656e27c01f4
Merlijn Busch <merlijn1busch@gmail.com>
2022-03-16 16:53:04 +0100

    fixed nav css

--- resources/js/admin/scripts/HandleSideNavigation.js
+++ resources/js/admin/scripts/HandleSideNavigation.js
@@ -1,5 +1,9 @@
 export default function initAddEventBtn() {
     const addEventBtn = document.querySelector('.partials-admin-layout-sidenav-btn-add-events');
+
+    if(addEventBtn === null)
+        return;
+
     addEventBtn.addEventListener('click', () => {
         window.dispatchEvent(new CustomEvent("modal-create-event", {detail: true}))
     })
