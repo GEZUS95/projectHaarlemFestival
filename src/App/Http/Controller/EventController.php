3cc87d906d40971a56c6bd7d1b3f2a973c4afa13
Merlijn Busch <merlijn1busch@gmail.com>
2022-03-24 21:04:38 +0100

    setup event overview page query

--- src/App/Http/Controller/EventController.php
+++ src/App/Http/Controller/EventController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controller;
 
 use App\Model\Event;
+use App\Model\Location;
+use App\Model\Performer;
 use Exception;
 use Matrix\BaseController;
 use Symfony\Component\HttpFoundation\Request;
@@ -20,11 +22,29 @@ class EventController extends BaseController
             ->where("title", "=", $title)
             ->with("programs")
             ->with("programs.items")
-            ->with("programs.items.locations")
+            ->with("programs.items.location")
             ->with("programs.items.performer")
-            ->get();
+            ->first();
 
-        return $this->render("partials.event.index", ["event" => $event]);
+        $locationsId = [];
+        foreach ($event->programs as $program) {
+            foreach ($program->items as $item) {
+                if(!in_array($item->location->id, $locationsId))
+                    array_push($locationsId, $item->location->id);
+            }
+        }
+        $locations = Location::findMany($locationsId);
+
+        $performersId = [];
+        foreach ($event->programs as $program) {
+            foreach ($program->items as $item) {
+                if(!in_array($item->performer->id, $performersId))
+                    array_push($performersId, $item->performer->id);
+            }
+        }
+        $performers = Performer::findMany($performersId);
+
+        return $this->render("partials.event.index", ["event" => $event, "locations" => $locations, "performers" => $performers]);
     }
 
 }
