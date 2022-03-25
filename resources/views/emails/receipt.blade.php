384e3e1a2a0589de6bf294f46e96a985722ea456
GEZUS95 <ceesgribnau@hotmail.com>
2022-03-25 18:59:04 +0100

    added receipt email blade with custom styles

---
+++ resources/views/emails/receipt.blade.php
@@ -0,0 +1,52 @@
+<html>
+<body>
+
+<h1 class="title">Haarlem Festival</h1>
+<h1 class="subtitle" >Hi Testpersoon!</h1>
+
+<p>Thanks for your order!<br>This is your confirmation email and a quick summary of the tickets you ordered!<br>
+    Your tickets will be below in the form of a QR-code!</p>
+<p>Here goes a quick overview of someone's
+    order but yeah I guess that it is easier to yoink the code that we will use on the main page for this</p>
+<p>Below you will find unique QR-code, you
+    will be able to use this code at the entrance of any event.<br>Put the generated qr-cde here and we gucci</p>
+
+<footer>This email was sent to you to inform
+    that an order has been placed using your account.<br>The email-address that this email has been sent from cannot
+    receive replies.<br>Don't want automated emails? Click <a href="www.haarlemfestival.com/opt_out">here</a> to opt out<br>For
+    help, visit: www.haarlemfestival.com/contact</footer>
+</body>
+</html>
+
+<style>
+    @import url('https://fonts.googleapis.com/css?family=Roboto');
+    body {
+        font-family: Roboto, serif;
+        margin: 0;
+        padding: 0;
+    }
+    p{
+        background-color:white;
+        color:#222222;
+        text-align:center;
+    }
+    footer{
+        font-size: x-small;
+        background-color:#ECEFF1;
+        text-align:center;
+        padding:2%
+    }
+    .title{
+        font-size:xx-large;
+        color:white;
+        background-color:#1A222A;
+        text-align:center;
+        padding:1%;
+    }
+    .subtitle{
+        background-color:white;
+        color:black;
+        text-align:center;
+        padding:20px;
+    }
+</style>
