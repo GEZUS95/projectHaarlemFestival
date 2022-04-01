2f79e1e2a7c534d138083a42143d84f490c35444
GEZUS95 <ceesgribnau@hotmail.com>
2022-04-01 18:43:30 +0200

    added captcha

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,13 +2,21 @@
 @section('content')
 <h1>Register</h1>
 
-<form action="{{\Matrix\Managers\RouteManager::getUrlByRouteName("register_post")}}" method="post">
+<form action="{{\Matrix\Managers\RouteManager::getUrlByRouteName("register_post")}}" id="register" method="post">
     <input type="hidden" name="token" value="{{\Matrix\Managers\SessionManager::getSessionManager()->get("register_form_csrf_token")}}">
     Name: <input name="name" required><br>
     Email: <input type="email" name="email" required><br>
     Email Confirm: <input type="text" name="email_confirmation" required><br>
     Password: <input type="password" name="password" required><br>
     Password Confirm: <input type="password" name="password_confirmation" required><br>
-    <input type="submit">
+    <button class="g-recaptcha"
+            data-sitekey="{{$_ENV['GOOGLEPUBKEY']}}"
+            data-callback='onSubmit'
+            data-action='submit'>Submit</button>
 </form>
+<script src="https://www.google.com/recaptcha/api.js">
+    function onSubmit(token) {
+        document.getElementById("register").submit();
+    }
+</script>
 @endsection
