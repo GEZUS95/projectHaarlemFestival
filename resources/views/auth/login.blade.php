0f0355af48f08d165c58f8e986933b3e3f7f43c1
GEZUS95 <ceesgribnau@hotmail.com>
2022-04-04 14:10:10 +0200

    password field

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
             <label for="email">Email:</label>
             <input type="text" name="email" id="email"><br>
             <label for="password">Password:</label>
-            <input type="text" name="password" id="password"><br>
+            <input type="password" name="password" id="password"><br>
             <div class="auth-layout-btn">
                 <input class="btn" type="submit">
             </div>
